c2c4152ea4cf66349d5cdc74a22558d177cda4e5
Rui <872715188@qq.com>
2022-03-31 23:03:07 +0800

    [其他提交] - 新增菜单图标选择

--- my-admin-front/src/views/system/menu/index.vue
+++ my-admin-front/src/views/system/menu/index.vue
@@ -36,14 +36,8 @@
               </el-form-item>
             </el-col>
             <el-col :span="12">
-              <el-form-item label="菜单图标" required>
-                <el-popover
-                  placement="bottom"
-                  trigger="click"
-                >
-                  <Icons />
-                  <el-input slot="reference" v-model="menuForm.icon" placeholder="请输入菜单名称" readonly />
-                </el-popover>
+              <el-form-item label="菜单图标">
+                <el-input v-model="menuForm.icon" placeholder="请输入菜单名称" readonly @click.native="iconDrawer = true" />
               </el-form-item>
             </el-col>
           </el-row>
@@ -98,11 +92,6 @@
       :tree-props="{children: 'children'}"
     >
       <el-table-column
-        prop="id"
-        label="ID"
-        width="180"
-      />
-      <el-table-column
         prop="name"
         label="菜单名称"
         width="180"
@@ -110,49 +99,110 @@
       <el-table-column
         prop="path"
         label="菜单路径"
+        align="center"
         width="180"
       />
       <el-table-column
         prop="component"
         label="菜单组件"
+        align="center"
+        width="180"
       />
       <el-table-column
         prop="componentName"
         label="组件名称"
+        align="center"
+        width="120"
       />
       <el-table-column
         prop="icon"
         label="菜单图标"
-      />
+        align="center"
+        width="50"
+      >
+        <template slot-scope="scope">
+          <svg-icon v-if="scope.row.icon && scope.row.icon.substr(0, 2) !== 'el'" :icon-class="scope.row.icon" />
+          <i v-else-if="scope.row.icon" :class="scope.row.icon" />
+        </template>
+      </el-table-column>
       <el-table-column
         prop="hidden"
         label="是否隐藏"
+        align="center"
+        width="80"
       />
       <el-table-column
         prop="alwaysShow"
         label="一直显示"
+        align="center"
+        width="80"
       />
       <el-table-column
         prop="authority"
         label="权限标识"
+        align="center"
       />
     </el-table>
+    <div>
+      <el-drawer
+        :visible.sync="iconDrawer"
+        direction="rtl"
+        :show-close="false"
+        :with-header="false"
+      >
+        <el-tabs type="border-card" class="icon-tabs">
+          <el-tab-pane label="自定义图标" style="overflow: auto">
+            <div class="grid">
+              <div v-for="item of svgIcons" :key="item" @click="handleClipboard(generateIconCode(item),$event)">
+                <el-tooltip placement="top">
+                  <div slot="content">
+                    {{ generateIconCode(item) }}
+                  </div>
+                  <div class="icon-item">
+                    <svg-icon :icon-class="item" class-name="disabled" />
+                    <span>{{ item }}</span>
+                  </div>
+                </el-tooltip>
+              </div>
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="Element-UI 图标">
+            <div class="grid">
+              <div v-for="item of elementIcons" :key="item" @click="handleClipboard(generateElementIconCode(item),$event)">
+                <el-tooltip placement="top">
+                  <div slot="content">
+                    {{ generateElementIconCode(item) }}
+                  </div>
+                  <div class="icon-item">
+                    <i :class="'el-icon-' + item" />
+                    <span>{{ item }}</span>
+                  </div>
+                </el-tooltip>
+              </div>
+            </div>
+          </el-tab-pane>
+        </el-tabs>
+      </el-drawer>
+    </div>
   </div>
 </template>
 
 <script>
 import { getMenus } from '@/api/menu'
-import Icons from '@/components/icons'
+// 菜单图标
+import clipboard from '@/utils/clipboard'
+import svgIcons from '@/assets/icons/svg-icons'
+import elementIcons from '@/assets/icons/element-icons'
 
 export default {
   name: 'Menu',
-  components: {
-    Icons
-  },
   data() {
     return {
       addFormOpen: false,
       loading: false,
+      iconDrawer: false,
+      svgIcons,
+      elementIcons,
       menuForm: {
         // 菜单名称
         name: '',
@@ -195,8 +245,14 @@ export default {
         this.loading = false
       }
     },
-    showIconSelector() {
-
+    generateIconCode(symbol) {
+      return `<svg-icon icon-class="${symbol}" />`
+    },
+    generateElementIconCode(symbol) {
+      return `<i class="el-icon-${symbol}" />`
+    },
+    handleClipboard(text, event) {
+      clipboard(text, event)
     }
   }
 }
@@ -210,4 +266,51 @@ export default {
 .switch-item {
   padding-top: 7px;
 }
+
+::v-deep .el-tabs__content {
+  height: calc(100vh - 39px);
+  overflow-y: auto;
+}
+
+::v-deep .el-tabs__content::-webkit-scrollbar {
+  display: none;
+}
+
+//::v-deep .el-table th > .cell {
+//  text-align: center;
+//}
+
+//::v-deep .el-table .cell {
+//  text-align: center;
+//}
+
+.icon-tabs {
+  .grid {
+    position: relative;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  }
+
+  .icon-item {
+    margin: 10px;
+    height: 60px;
+    text-align: center;
+    width: 80px;
+    float: left;
+    font-size: 16px;
+    color: #24292e;
+    cursor: pointer;
+  }
+
+  span {
+    display: block;
+    font-size: 12px;
+    margin-top: 10px;
+  }
+
+  .disabled {
+    pointer-events: none;
+  }
+}
+
 </style>
